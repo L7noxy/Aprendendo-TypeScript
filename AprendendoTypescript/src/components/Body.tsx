dda4e80e68e68be42c49ac71c4ac69b330627905
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-02-14 20:55:44 -0300

    Criação do projeto em TypeScript

---
+++ AprendendoTypescript/src/components/Body.tsx
@@ -0,0 +1,16 @@
+import 'typescript';
+import './Body.css'
+
+function Body() {
+  return (
+    <>
+      <div>
+        <nav>
+            <p>Logo</p>     
+        </nav>
+      </div>
+    </>
+  )
+}
+
+export default Body
